754c199670c598551e6505d9f124b4a9123eeed0
Alexander Trauzzi <atrauzzi@gmail.com>
2016-06-23 20:36:06 -0500

    Evolving...

--- src/AuthAndApiAspnet4/Extensions.cs
+++ src/AuthAndApiAspnet4/Extensions.cs
@@ -6,11 +6,27 @@ namespace AuthAndApi.Aspnet4 {
     public static class Extensions {
 
         public static void SetAuthAndApiState(this HttpSessionStateBase session, State state) {
-            session[$"auth_and_api_{state.Key}"] = state;
+            session[$"auth-and-api:state:{state.Key}"] = state;
         }
 
         public static State GetAuthAndApiState(this HttpSessionStateBase session, string key) {
             return session[key] as State;
+        }
+
+        public static bool HasAuthAndApiState(this HttpSessionStateBase session, string key) {
+            return session.GetAuthAndApiState(key) != null;
+        }
+
+        public static bool HasStatelessBridge(this HttpSessionStateBase session) {
+            return session.GetStatelessBridge() != null;
+        }
+
+        public static State GetStatelessBridge(this HttpSessionStateBase session) {
+            return session["auth-and-api:state-bridge"] as State;
+        }
+
+        public static void SetStatelessBridge(this HttpSessionStateBase session, State state) {
+            session["auth-and-api:state-bridge"] = state;
         }
 
     }
---
+++ src/AuthAndApiAspnet4/ExternalLogin.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace AuthAndApi.Aspnet4 {
+
+    public class ExternalLogin {
+
+        [Required]
+        public string Code { get; set; }
+
+        public string State { get; set; }
+
+    }
+
+}
